1db04f5c92dbf9c66791cecd3d7cf9fdbbc81623
ayyazshah106-cloud <ayyazshah106@gmail.com>
2026-04-07 00:26:36 +0500

    update project

--- Todo_List_App/Todo_List_App/Html and Css/Application Form.js
+++ Todo_List_App/Todo_List_App/Html and Css/Application Form.js
@@ -1,7 +1,15 @@
-const firstName = document.getElementById("First_name");
+
+const submitBtn = document.getElementById("submit");
+const resetBtn = document.getElementById("reset");
+
+submitBtn.addEventListener("click", (e)=>{
+  e.preventDefault();
+
+const firstName = document.getElementById("First_name").value;
 const lastName = document.getElementById("Last_Name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
+const password_match = document.getElementById("password_match");
 const phoneNumber = document.getElementById("phone_number");
 const dateBirth = document.getElementById("date_brith");
 
@@ -21,5 +29,13 @@ const experienceLevel = document.getElementById("range");
 const aboutTextarea = document.getElementById("textarea");
 const termsAndConditions = document.getElementById("term_and_condition");
 
-const submitBtn = document.getElementById("submit");
-const resetBtn = document.getElementById("reset");
+console.log(firstName);
+
+const firstNameRegex = /^[A-Za-z]{3,20}$/;
+const passwordRegex = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[\W]).{8,20}/
+const phoneNumberRegex = /^(\+92|0)?3\d{9}$/;
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+})
+
+
