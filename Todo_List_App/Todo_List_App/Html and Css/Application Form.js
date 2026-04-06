a0e28f52d6159c88ee17c6a2ae19cac3fb9271a2
ayyazshah106-cloud <ayyazshah106@gmail.com>
2026-04-06 23:39:15 +0500

    update project

---
+++ Todo_List_App/Todo_List_App/Html and Css/Application Form.js
@@ -0,0 +1,25 @@
+const firstName = document.getElementById("First_name");
+const lastName = document.getElementById("Last_Name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const phoneNumber = document.getElementById("phone_number");
+const dateBirth = document.getElementById("date_brith");
+
+const genderMale = document.getElementById("Gender"); // Note: HTML me sab radio ka same id hai, ideally unique ID deni chahiye
+const genderFemale = document.getElementById("Gender");
+const genderOther = document.getElementById("Gender");
+
+
+
+
+const skillWeb = document.getElementById("skills"); // 
+
+const fileUpload = document.getElementById("file_upload");
+const timeInput = document.getElementById("time");
+const experienceLevel = document.getElementById("range");
+
+const aboutTextarea = document.getElementById("textarea");
+const termsAndConditions = document.getElementById("term_and_condition");
+
+const submitBtn = document.getElementById("submit");
+const resetBtn = document.getElementById("reset");
